239b9f301262970989b1de593f20d4e0a43eeac3
Kamila karoliny dos Santos <kamila_k_santos@estudante.sesisenai.org.br>
2025-09-09 11:49:45 -0300

    add - estrutura do futebol_em_crud

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+</div> <!-- fecha container -->
+
+<footer class="bg-dark text-light text-center py-3 mt-4 rodape">
+  <p>Futebol CRUD © <br> Por Kamila Santos <br> <?= date("Y") ?></p>
+</footer>
+
+<script src="/CRUD_Futebol/futebol_em_crud/assets/js/bootstrap.bundle.min.js"></script>
+<link rel="stylesheet" href="./style/style.css">
+</body>
+</html>
